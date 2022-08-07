4f2b4a5e33b4e4200aefa6e81524693ddaabecc2
Oleg Zhmaev <grasword6@gmail.com>
2022-08-07 19:37:37 +0300

    implemented tests for `Course section`

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,17 +1,40 @@
+/// <reference types="cypress-xpath" />
+
 describe("Main page", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000")
+    cy.wait(1000)
   })
 
-  it("The h1 contains the correct text", () => {
-    cy.get('[data-test="hero-heading"] > .block')
-      .should("exist")
-      .contains("Testing Next.js Applications with Cypress")
+  context("Hero Section", () => {
+    it("The h1 contains the correct text", () => {
+      cy.get('[data-test="hero-heading"] > .block')
+        .should("exist")
+        .contains("Testing Next.js Applications with Cypress")
+    })
+
+    it("The features on the home page are correct", () => {
+      cy.get("dt").eq(0).contains("4 Courses")
+      cy.get("dt").eq(1).contains("25+ Lessons")
+      cy.get("dt").eq(2).contains("Free and Open Source")
+    })
   })
 
-  it("The features on the home page are correct", () => {
-    cy.get("dt").eq(0).contains("4 Courses")
-    cy.get("dt").eq(1).contains("25+ Lessons")
-    cy.get("dt").eq(2).contains("Free and Open Source")
+  context("Course section", () => {
+    it("Testing Your First Next.js Application", () => {
+      // cy.xpath(`.//div[@data-test="course-0"]//a`).should("exist")
+      cy.getByData("course-0").find("a").contains("Get started").click()
+      cy.location("pathname").should("eq", "/testing-your-first-application")
+    })
+
+    it("Testing Foundations", () => {
+      cy.getByData("course-1").find("a").contains("Get started").click()
+      cy.location("pathname").should("eq", "/testing-foundations")
+    })
+
+    it("Cypress Fundamentals", () => {
+      cy.getByData("course-2").find("a").contains("Get started").click()
+      cy.location("pathname").should("eq", "/cypress-fundamentals")
+    })
   })
 })
